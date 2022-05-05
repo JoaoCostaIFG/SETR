172ebe4c14cc1aeca8447bd20e224ad6066e7817
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-05 01:04:49 +0100

    Add: one more task taskating

--- uKernel/uKernel.ino
+++ uKernel/uKernel.ino
@@ -10,7 +10,7 @@ Mutex* mutex1 = Sched_CreateMutex();
 
 static Task* task1 = new Task(&task1Func, (void*) 0, 43, 3, 0, 3);
 static Task* task2 = new Task(&task2Func, (void*) 0, 43, 3, 0, 3);
-//static Task* task3 = new Task(&task3Func, (void*) 0, 200, 3, 0, 3);
+static Task* task3 = new Task(&task3Func, (void*) 0, 36, 3, 0, 3);
 //static Task* task4 = new Task(&task4Func, (void*) 0, 43, 1, 0, 1);
 
 ISR(SCHEDULER_ISR, ISR_NAKED) __attribute__ ((hot, flatten));
@@ -38,13 +38,13 @@ void setup() {
 
   assert(Sched_Add(task1) == 0);
   assert(Sched_Add(task2) == 0);
-  //assert(Sched_Add(task3) == 0);
+  assert(Sched_Add(task3) == 0);
 
   Sched_Start();
 
   free(task1);
   free(task2);
-  //free(task3);
+  free(task3);
   //free(task4);
 
   free(mutex1);
