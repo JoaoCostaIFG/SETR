8002f7f8689e4dcb00eeec1a1d6e73232f381243
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-06 21:42:53 +0100

    Doc: document Map class.

--- uKernel/include/Map.h
+++ uKernel/include/Map.h
@@ -8,25 +8,39 @@
 
 #define MAP_INIT_SIZE 2
 
-template <typename KEY, typename VALUE> class Map {
+/**
+ * This isn't exactly what you expected when you see "Map": it isn't implemented using a set.
+ * It is implemented using a list. Operations that should be O(1) are most time O(n).
+ * This is ok, because the expected number of elements for our application is around 1 or 2 (maybe 3 in special cases).
+ * @tparam KEY      The type of the map keys
+ * @tparam VALUE    The type of the map values
+ */
+template<typename KEY, typename VALUE>
+class Map {
 public:
+  /** A map element. Stores a key and its corresponding value. */
   typedef struct {
     KEY key;
     VALUE value;
   } MapElement;
 
 private:
-  MapElement **data;
+  /** Array of map elements. */
+  MapElement** data;
+  /** The number of elements currently in the map. */
   size_t size;
+  /** Current maximum size of the map. Used internally to calculate when to grow the @data container */
   size_t maxSize;
 
+  /** Duplicates the size of the @data container. Used internally. */
   void increaseSize() {
     this->maxSize *= 2;
-    this->data = (MapElement **)realloc(this->data,
-                                        this->maxSize * sizeof(MapElement *));
+    this->data = (MapElement**) realloc(this->data,
+                                        this->maxSize * sizeof(MapElement*));
     assertCond(this->data != nullptr, F("Failed to re-allocate map memory"));
   }
 
+  /** Appends a new element to the @data container. Used internally. */
   void add(KEY k, VALUE v) {
     if (this->size >= this->maxSize)
       increaseSize();
@@ -37,7 +51,7 @@ public:
   Map() {
     this->maxSize = MAP_INIT_SIZE;
     this->size = 0;
-    this->data = (MapElement **)malloc(MAP_INIT_SIZE * sizeof(MapElement *));
+    this->data = (MapElement**) malloc(MAP_INIT_SIZE * sizeof(MapElement*));
     assertCond(this->data != nullptr, F("Failed to allocate map memory"));
   }
 
@@ -48,17 +62,20 @@ public:
     free(this->data);
   }
 
+  /** Returns the number of elements in the Map. */
   size_t getSize() const { return this->size; }
 
-  MapElement *at(size_t i) const {
+  /** Get an element by index. Used for iterating the map. */
+  MapElement* at(size_t i) const {
     if (i >= this->size)
       return nullptr;
     return this->data[i];
   }
 
-  MapElement *get(KEY k) const {
+  /** Get an element by key. */
+  MapElement* get(KEY k) const {
     for (size_t i = 0; i < this->size; ++i) {
-      MapElement *elem = this->data[i];
+      MapElement* elem = this->data[i];
       if (elem->key == k)
         return elem;
     }
@@ -66,8 +83,9 @@ public:
     return nullptr;
   }
 
+  /** Set the value for a key. */
   void set(KEY k, VALUE v) {
-    MapElement *elem = this->get(k);
+    MapElement* elem = this->get(k);
     if (elem == nullptr) {
       this->add(k, v);
     } else {
@@ -75,10 +93,11 @@ public:
     }
   }
 
+  /** Remove the given key from the container. */
   void remove(KEY k) {
     size_t i;
     for (i = 0; i < this->size; ++i) {
-      MapElement *elem = this->data[i];
+      MapElement* elem = this->data[i];
       if (elem->key == k) {
         free(elem);
         break;
